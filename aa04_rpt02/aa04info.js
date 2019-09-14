f5a552d92c4ab816f52a85bbda8a862f55a6404d
akasia1 <42956354+akasia1@users.noreply.github.com>
2019-09-14 11:36:41 +0900

    Add files via upload

---
+++ aa04_rpt02/aa04info.js
@@ -0,0 +1,11 @@
+//aa04info.js
+
+module.exports = function (id, name, phone, major)
+{
+	console.log("ID "+ id);
+	console.log("Name "+ name);
+	console.log("Phone "+ phone);
+	console.log("Major "+ major + "\n");
+
+}
+
---
+++ aa04_rpt02/index_aa04.js
@@ -0,0 +1,8 @@
+//index_aa04.js
+var info = require("./aa04info.js");
+
+info("aa04","kimeheechan","010-0000-0000","computer simulation");
+
+
+
+
